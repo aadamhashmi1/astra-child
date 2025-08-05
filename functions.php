4a94e01dc5c137c22228d38b575fb9af75ea0310
aadamhashmi1 <muhammadaadamhashmi@gmail.com>
2025-08-05 22:13:00 +0500

    finalized

--- functions.php
+++ functions.php
@@ -1,18 +1,16 @@
 <?php
 
-function astra_child_enqueue_assets() {
-    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap', false);
+function astra_child_enqueue_styles() {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('child-style', get_stylesheet_uri(), ['parent-style']);
-    wp_enqueue_script('scroll-top', get_stylesheet_directory_uri() . '/js/scroll-top.js', [], null, true);
 }
-
-add_action('wp_enqueue_scripts', 'astra_child_enqueue_assets');
+add_action('wp_enqueue_scripts', 'astra_child_enqueue_styles');
 
 function astra_child_register_sidebar() {
     register_sidebar([
         'name' => __('Custom Sidebar', 'astra-child'),
         'id' => 'custom-sidebar',
+        'description' => __('A custom widget area in the Astra child theme.', 'astra-child'),
         'before_widget' => '<div class="custom-widget">',
         'after_widget' => '</div>',
         'before_title' => '<h3 class="widget-title">',
